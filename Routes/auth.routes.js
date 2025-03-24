71754a609be8126c63f54f8d6061d26b913b4fa3
Santhosh <santhosharumugam2k3@gmail.com>
2025-03-24 22:25:25 +0530

    Latest1

--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { forgotPassword, resetPassword } = require("../Controllers/auth.controller");
 const router = express.Router();
 
-router.post("/forgot-password", forgotPassword);
-router.post("/reset-password/:token", resetPassword);
+router.post("https://passwordapp-server.onrender.com/forgot-password", forgotPassword);
+router.post("https://passwordapp-server.onrender.com/reset-password/:token", resetPassword);
 
 module.exports = router;
